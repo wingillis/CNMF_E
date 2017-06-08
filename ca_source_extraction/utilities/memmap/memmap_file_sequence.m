b62d601f13bdfd696a97c259ac76f160c4e5307f
wingillis <win.gillis@gmail.com>
2017-06-08 13:26:23 -0400

    Added glob option for different files in a folder

--- ca_source_extraction/utilities/memmap/memmap_file_sequence.m
+++ ca_source_extraction/utilities/memmap/memmap_file_sequence.m
@@ -1,6 +1,6 @@
-function data = memmap_file_sequence(foldername)
+function data = memmap_file_sequence(foldername, fileglob)
 
-% read a sequence of stacked tiff arrays, reshapes it to 2d array and saves it a mat file that can be memory mapped. 
+% read a sequence of stacked tiff arrays, reshapes it to 2d array and saves it a mat file that can be memory mapped.
 % The file is saved both in its original format and as a reshaped 2d matrix
 
 % INPUTS
@@ -15,10 +15,15 @@ function data = memmap_file_sequence(foldername)
 % Author: Eftychios A. Pnevmatikakis, Simons Foundation, 2016
 
 nY = Inf;
-files = dir([foldername,'/*.tif']);
+
+if nargin > 1
+  files = dir([foldername, fileglob]);
+else
+  files = dir([foldername,'/*.tif']);
+end
 filesCell = struct2cell(files);
 names = filesCell(1,:);
-[names, idx] = natsortfiles(names);
+[~, idx] = natsortfiles(names);
 T = 0;
 data = matfile([foldername,'/',files(1).name(1:end-8),'.mat'],'Writable',true);
 tt1 = tic;
@@ -29,7 +34,7 @@ for i = 1:length(files)
     Yr = reshape(Y,prod(sizY(1:end-1)),[]);
     nY = min(min(Yr(:)),nY);
     if length(sizY) == 3
-        data.Y(1:sizY(1),1:sizY(2),T+(1:size(Yr,2))) = Y;        
+        data.Y(1:sizY(1),1:sizY(2),T+(1:size(Yr,2))) = Y;
     elseif legnth(sizY) == 4
         data.Y(1:sizY(1),1:sizY(2),1:sizY(3),T+(1:size(Yr,2))) = Y;
     end
