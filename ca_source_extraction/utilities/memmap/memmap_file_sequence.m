a37b54b5b3f5c1de586a87c825127895512de62a
wingillis <win.gillis@gmail.com>
2017-07-18 16:11:12 -0400

    Added output so user knows which file is being read into the system

--- ca_source_extraction/utilities/memmap/memmap_file_sequence.m
+++ ca_source_extraction/utilities/memmap/memmap_file_sequence.m
@@ -28,19 +28,20 @@ T = 0;
 data = matfile([foldername,'/',files(1).name(1:end-8),'.mat'],'Writable',true);
 tt1 = tic;
 for i = 1:length(files)
-    filename = [foldername,'/',files(idx(i)).name];
-    Y = bigread2(filename);
-    sizY = size(Y);
-    Yr = reshape(Y,prod(sizY(1:end-1)),[]);
-    nY = min(min(Yr(:)),nY);
-    if length(sizY) == 3
-        data.Y(1:sizY(1),1:sizY(2),T+(1:size(Yr,2))) = Y;
-    elseif legnth(sizY) == 4
-        data.Y(1:sizY(1),1:sizY(2),1:sizY(3),T+(1:size(Yr,2))) = Y;
-    end
-    data.Yr(1:prod(sizY(1:end-1)),T+(1:size(Yr,2))) = Yr;
-    T = sizY(end) + T;
-    toc(tt1);
+  fprintf('Reading in file %s\n', files(idx(i)).name);
+  filename = [foldername,'/', files(idx(i)).name];
+  Y = bigread2(filename);
+  sizY = size(Y);
+  Yr = reshape(Y,prod(sizY(1:end-1)),[]);
+  nY = min(min(Yr(:)),nY);
+  if length(sizY) == 3
+    data.Y(1:sizY(1),1:sizY(2),T+(1:size(Yr,2))) = Y;
+  elseif legnth(sizY) == 4
+    data.Y(1:sizY(1),1:sizY(2),1:sizY(3),T+(1:size(Yr,2))) = Y;
+  end
+  data.Yr(1:prod(sizY(1:end-1)),T+(1:size(Yr,2))) = Yr;
+  T = sizY(end) + T;
+  toc(tt1);
 end
 sizY(end) = T;
 data.sizY = sizY;
